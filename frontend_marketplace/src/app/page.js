8bea0a34b03eebc62f6ad8200d09a35f0dad2e49
ShrinathNR <nr.shrinath@gmail.com>
2023-09-16 01:07:21 +0530

    learnt nextjs

---
+++ frontend_marketplace/src/app/page.js
@@ -0,0 +1,9 @@
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <div>
+      <h1 className = "text-2xl">marketplace program</h1>
+    </div>
+  )
+}
---
+++ frontend_marketplace/src/app/posts/page.js
@@ -0,0 +1,8 @@
+export default function Post() {
+    return (
+      <div>
+        <h1>post 1</h1>
+      </div>
+    )
+  }
+  
