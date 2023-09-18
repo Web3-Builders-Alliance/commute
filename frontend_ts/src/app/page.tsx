2c5744a907f2debae638be26b6bc4d507eb777cb
ShrinathNR <nr.shrinath@gmail.com>
2023-09-18 16:40:16 +0530

    added context to all components

--- frontend_ts/src/app/page.tsx
+++ frontend_ts/src/app/page.tsx
@@ -1,44 +1,21 @@
 'use client';
 import React, { FC, useMemo } from 'react';
-import { ConnectionProvider, WalletProvider } from '@solana/wallet-adapter-react';
-import { WalletAdapterNetwork } from '@solana/wallet-adapter-base';
 import {
-    WalletModalProvider,
     WalletDisconnectButton,
     WalletMultiButton
 } from '@solana/wallet-adapter-react-ui';
-import { UnsafeBurnerWalletAdapter } from '@solana/wallet-adapter-wallets';
+import { SendSOL } from '@/components/SendSOL';
 
 // Default styles that can be overridden by your app
 require('@solana/wallet-adapter-react-ui/styles.css');
 
 
 export default function Home() {
-  
-  
-  const network = WalletAdapterNetwork.Devnet;
-  const endpoint = `https://api.devnet.solana.com`;
-  const wallets = useMemo(
-    () => [
-
-        new UnsafeBurnerWalletAdapter(),
-    ],
-    [network]
-);
-  
-  
   return (
-    <div className='flex justify-between'>
+    <div >
       <h1 className='text-2xl'>market place</h1>
-      <ConnectionProvider endpoint={endpoint}>
-            <WalletProvider wallets={wallets} autoConnect>
-                <WalletModalProvider>
-                    <WalletMultiButton />
-                    <WalletDisconnectButton />
-                    { /* Your app's components go here, nested within the context providers. */ }
-                </WalletModalProvider>
-            </WalletProvider>
-        </ConnectionProvider>
+      <WalletMultiButton />
+      <WalletDisconnectButton/>
       
     </div>
   )
